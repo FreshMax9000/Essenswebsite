1d89c924e3c10bceea79d3254aefa0e8a7bdcc8f
Philipp Reimund <philipp@hmreimund.de>
2020-04-01 23:24:29 +0200

    created new DjangoApp users - login - logout - register - profile

--- foodplaner/foodApp/models.py
+++ foodplaner/foodApp/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Ingredients(models.Model):
     name = models.CharField(max_length=100)
+    unit = models.CharField(max_length=100,default="")
 
     def __str__(self):
         return self.name
