1f66130e9ea1c860b770c85420a64b8c3391373a
sassas98 <sandra.gall@outlook.de>
2020-04-03 21:37:48 +0200

    Wochenplan, Mein Profil, Homepage hinzugefügt
    Wochenplan, Mein Profil, Homepage hinzugefügt

--- foodplaner/foodApp/views.py
+++ foodplaner/foodApp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 
 from .models import Recipes
+from users.models import Profile
 
 def home(request):
     return render(request, 'foodApp/home.html')
@@ -13,3 +14,11 @@ class RecipesListView(generic.ListView):
 
 class RecipesDetailView(generic.DetailView):
     model = Recipes
+
+def MyProfil(request):
+    Recipe = [Recipes.title]
+    return render(request, 'foodApp/myProfil.html', {'Recip' : Recipe})
+
+
+def Wochenplan(request):
+    return render(request, 'foodApp/Wochenplan.html')
